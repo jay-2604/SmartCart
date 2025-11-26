e04a7a1fe53e7d46ceff9c12688dba3fcea0e0d8
jay-2604 <jayanthvarmakjv@gmail.com>
2025-11-27 02:03:01 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,23 @@
+# config.py
+# ------------------------------------------
+# Stores all configuration settings
+# ------------------------------------------
+
+SECRET_KEY = "abc123"
+
+# MySQL Database
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "root"
+DB_NAME = "smartcart2_db"
+
+# Email SMTP Settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'jayanthvarmakjv@gmail.com'
+MAIL_PASSWORD = 'gaxo zhkj qzjj fudy'   # Gmail App Password
+
+
+RAZORPAY_KEY_ID = "rzp_test_RjUcgp5R6tfq2m"
+RAZORPAY_KEY_SECRET = "ZBnUQSKaNqAUky8ubAaN6SOy"
